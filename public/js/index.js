8f55c41c19c6fecdcddbfa4e541adb07d2b0fd08
victorsteven <chikodi543@gmail.com>
2018-10-14 21:54:16 +0100

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -25,10 +25,21 @@ var socket = io(); //initialise/create connection
 
 socket.on('newMessage', function(message){
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('new Message entered', message);
-    var li = $('<li></li>'); //used jquery to create an element
-    li.text(`${message.from}: ${message.text} at ${formattedTime}`);
-    $('#messages').append(li);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+
+    });
+    $('#messages').append(html);
+
+    // let formattedTime = moment(message.createdAt).format('h:mm a');
+    // console.log('new Message entered', message);
+    // var li = $('<li></li>'); //used jquery to create an element
+    // li.text(`${message.from}: ${message.text} at ${formattedTime}`);
+    // $('#messages').append(li);
+
 })
 
 // socket.emit('createMessage', {
@@ -39,14 +50,23 @@ socket.on('newMessage', function(message){
 // });
 socket.on('newLocationMessage', function(message){
     let formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    li.append(" " + formattedTime);
-    $('#messages').append(li);
+    let template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        // text: message.text,
+        from: message.from,
+        createdAt: formattedTime,
+        url: message.url
+    });
+    $('#messages').append(html);
+
+    // var li = $('<li></li>');
+    // var a = $('<a target="_blank">My current location</a>');
+    // li.text(`${message.from}: `);
+    // a.attr('href', message.url);
+    // li.append(a);
+    // li.append(" " + formattedTime);
+    // $('#messages').append(li);
 })
 
 $('#message-form').on('submit', function(e){
